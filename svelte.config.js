589adbcf5e987d34051e80f93eb449a054665f87
Jason Li <jasonmli@outlook.com>
2026-02-25 21:16:09 +0800

    Change bundling

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { adapter: adapter(), output: { bundleStrategy: "single" } } };
 
 export default config;
